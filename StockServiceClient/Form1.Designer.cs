75c3a46bbcf49a5a106f070dead70f61c0842965
carlossper <c_m_per@hotmail.com>
2016-06-01 17:08:49 +0100

    - Added async call to object method to add order

--- StockServiceClient/Form1.Designer.cs
+++ StockServiceClient/Form1.Designer.cs
@@ -28,38 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.Email = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.BuySell = new System.Windows.Forms.ComboBox();
+            this.submit_button = new System.Windows.Forms.Button();
+            this.Amount = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.Amount)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // NameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(115, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(366, 22);
-            this.textBox1.TabIndex = 0;
+            this.NameTextBox.Location = new System.Drawing.Point(115, 55);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(366, 22);
+            this.NameTextBox.TabIndex = 0;
             // 
-            // textBox2
+            // Email
             // 
-            this.textBox2.Location = new System.Drawing.Point(115, 95);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(366, 22);
-            this.textBox2.TabIndex = 1;
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(115, 175);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(366, 22);
-            this.textBox4.TabIndex = 3;
+            this.Email.Location = new System.Drawing.Point(115, 95);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(366, 22);
+            this.Email.TabIndex = 1;
             // 
             // label1
             // 
@@ -105,43 +99,49 @@
             this.label5.Size = new System.Drawing.Size(0, 17);
             this.label5.TabIndex = 9;
             // 
-            // comboBox1
+            // BuySell
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Buy",
-            "Sell"});
-            this.comboBox1.Location = new System.Drawing.Point(115, 134);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 24);
-            this.comboBox1.TabIndex = 10;
+            this.BuySell.FormattingEnabled = true;
+            this.BuySell.Location = new System.Drawing.Point(115, 134);
+            this.BuySell.Name = "BuySell";
+            this.BuySell.Size = new System.Drawing.Size(121, 24);
+            this.BuySell.TabIndex = 10;
             // 
-            // button1
+            // submit_button
             // 
-            this.button1.Location = new System.Drawing.Point(140, 223);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(271, 23);
-            this.button1.TabIndex = 11;
-            this.button1.Text = "Submit button";
-            this.button1.UseVisualStyleBackColor = true;
+            this.submit_button.Location = new System.Drawing.Point(140, 223);
+            this.submit_button.Name = "submit_button";
+            this.submit_button.Size = new System.Drawing.Size(271, 23);
+            this.submit_button.TabIndex = 11;
+            this.submit_button.Text = "Submit button";
+            this.submit_button.UseVisualStyleBackColor = true;
+            this.submit_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Amount
+            // 
+            this.Amount.Location = new System.Drawing.Point(116, 178);
+            this.Amount.Name = "Amount";
+            this.Amount.Size = new System.Drawing.Size(120, 22);
+            this.Amount.TabIndex = 12;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(542, 280);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.Amount);
+            this.Controls.Add(this.submit_button);
+            this.Controls.Add(this.BuySell);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.NameTextBox);
             this.Name = "Form1";
             this.Text = "Counter";
+            ((System.ComponentModel.ISupportInitialize)(this.Amount)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -149,16 +149,16 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox Email;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox BuySell;
+        private System.Windows.Forms.Button submit_button;
+        private System.Windows.Forms.NumericUpDown Amount;
     }
 }
 
--- StockServiceClient/Form1.cs
+++ StockServiceClient/Form1.cs
@@ -17,8 +17,23 @@ namespace StockServiceClient
         public Form1()
         {
             InitializeComponent();
-            this.proxy = new StockService.StockDirectoryClient();
-            this.proxy.ExecuteOrder(1, 3f);
+            this.BuySell.Items.Add(StockService.StockOrder.OrderType.Purchase);
+            this.BuySell.Items.Add(StockService.StockOrder.OrderType.Sale);
+            //this.proxy = new StockService.StockDirectoryClient();
+            //this.proxy.ExecuteOrder(1, 3f);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(this.submit_button.InvokeRequired)
+            {
+                StockService.StockOrder.OrderType type = (StockService.StockOrder.OrderType)this.BuySell.SelectedItem;
+               proxy.OrderStockAsync(this.CompanyName, (int)this.Amount.Value, type, this.Email.Text);
+            }
+            else
+            {
+
+            }
         }
     }
 }
